1e88427ac0e25ee1a55ebbe13f83bcb55fac3b37
Eko35 <33695661+Eko35@users.noreply.github.com>
2019-05-14 20:47:54 +0300

    Add files via upload

---
+++ astar/floattest.java
@@ -0,0 +1,24 @@
+/**
+ * @(#)floattest.java
+ *
+ *
+ * @author 
+ * @version 1.00 2017/12/12
+ */
+
+public class floattest {
+        
+    /**
+     * Creates a new instance of <code>floattest</code>.
+     */
+    public floattest() {
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        float x = 80.0f;
+        System.out.println((int)(x/40));
+    }
+}
